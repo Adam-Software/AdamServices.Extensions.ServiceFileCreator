d1c0ac641c5ba7884d86ff7e825b399ea978544f
Alexey Nesterov <vertigra.developer@gmail.com>
2025-04-04 15:16:13 +1000

    Added: + TestApp + Package description

---
+++ src/ServiceFileCreator.TestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using ServiceFileCreator.Extensions;
+
+namespace ServiceFileCreator.TestApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+
+                  .ConfigureServices((context, services) =>
+                  {
+                      services.AddAdamServiceFileCreator();
+                  })
+                  .Build();
+
+            host.UseAdamServiceFileCreator();
+            await host.RunAsync();
+        }
+    }
+}
